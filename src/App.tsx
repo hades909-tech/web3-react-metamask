5a57757640b41cd5e266a927d0e4bc16aa70a977
velvettouch1004 <velvettouch1004@gmail.com>
2024-11-30 03:27:58 -0500

    initial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ConnectWallet from "./components/connectWallet";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <ConnectWallet />
+    </div>
+  );
+};
+
+export default App;
